71ce11031d57c5f2ebab51f76881a83517bb2224
ika25 <lomiika1990@yahoo.com>
2021-02-18 19:59:35 +0000

    update google maps

--- hikingapp-2.0/src/app/pages/places/places.page.ts
+++ hikingapp-2.0/src/app/pages/places/places.page.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker,
+  Environment
+} from '@ionic-native/google-maps'; 
+import { Platform } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-places',
@@ -7,9 +19,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlacesPage implements OnInit {
 
-  constructor() { }
+  map: GoogleMap;
 
-  ngOnInit() {
+  constructor(private platform:Platform) { }
+
+  ionViewDidLoad() {
+    this.loadMap();
+  }
+
+  async ngOnInit() {
+    await this.platform.ready();
+    await this.loadMap();
+  }
+
+  loadMap() {
+
+    // This code is necessary for browser
+    Environment.setEnv({
+      'API_KEY_FOR_BROWSER_RELEASE': 'AIzaSyD6wz-2LPTEqkqAtgA_0Mk-IIraeM7WvoQ',
+      'API_KEY_FOR_BROWSER_DEBUG': 'AIzaSyD6wz-2LPTEqkqAtgA_0Mk-IIraeM7WvoQ'
+    });
+
+    
+
+    this.map = GoogleMaps.create('map_canvas');
+ 
   }
 
 }
