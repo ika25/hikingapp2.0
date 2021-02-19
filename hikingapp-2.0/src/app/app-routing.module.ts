66ec159379cb0a2c1b150b4726426c5dcaff0cde
ika25 <lomiika1990@yahoo.com>
2021-02-19 15:13:41 +0000

    Home page development

--- hikingapp-2.0/src/app/app-routing.module.ts
+++ hikingapp-2.0/src/app/app-routing.module.ts
@@ -9,6 +9,21 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+  },   {
+    path: 'places',
+    loadChildren: () => import('./pages/places/places.module').then( m => m.PlacesPageModule)
+  },
+  {
+    path: 'search',
+    loadChildren: () => import('./pages/search/search.module').then( m => m.SearchPageModule)
+  },
+  {
+    path: 'calories',
+    loadChildren: () => import('./pages/calories/calories.module').then( m => m.CaloriesPageModule)
+  },
+  {
+    path: 'record',
+    loadChildren: () => import('./pages/record/record.module').then( m => m.RecordPageModule)
   }
 
 ];
