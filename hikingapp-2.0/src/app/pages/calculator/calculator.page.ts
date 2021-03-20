2288b1e1d993d853a1e8f333e5845d5befe0af53
ika25 <lomiika1990@yahoo.com>
2021-03-20 11:43:12 +0000

    Updated calculator.page.ts

--- hikingapp-2.0/src/app/pages/calculator/calculator.page.ts
+++ hikingapp-2.0/src/app/pages/calculator/calculator.page.ts
@@ -38,9 +38,19 @@ export class CalculatorPage implements OnInit {
      var weight= parseInt(form.value['weight']);
      var bweight= parseInt(form.value['bweight']);
 //Multiply your body weight in kilograms (i.e. 60) by 0.035. Eg. 0.035 x 60 = 2.1
+var result = (weight+bweight)*0.035
 //Square your velocity (or speed) in metres per second, i.e. multiply it by itself. Eg. 1.4 x 1.4 = 1.96
+
+var hmins= parseInt(form.value['hmins']);
+var dist= parseInt(form.value['dist']);
+ var speed=(dist*1000)/(hmins*60);
+
+ var speed2 = speed*speed;
+
 //Divide the result by your height in metres (i.e. 1.6). Eg.1.96 ÷ 1.6 = 1.225
-     var calories = weight+bweight;
+
+
+     var calories = result*speed2*1000;
   
     const alert = await this.alertController.create({ 
       header: 'Calories Info!',
