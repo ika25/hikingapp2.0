7d9a2cb6e345c07face4c04e73891ee4a6aed524
ika25 <lomiika1990@yahoo.com>
2021-03-20 11:26:18 +0000

    calculating calories burned/arranging screen view

--- hikingapp-2.0/src/app/pages/calculator/calculator.page.ts
+++ hikingapp-2.0/src/app/pages/calculator/calculator.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AlertController } from 'node_modules_del/@ionic/angular';
+import { AlertController } from '@ionic/angular';
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-calculator',
@@ -7,9 +8,16 @@ import { AlertController } from 'node_modules_del/@ionic/angular';
   styleUrls: ['./calculator.page.scss'],
 })
 export class CalculatorPage implements OnInit {
+  ionicForm: FormGroup;
 
-  constructor(public alertController: AlertController) {}
+  constructor(public alertController: AlertController,public formBuilder: FormBuilder) {}
   ngOnInit(): void {
+
+
+    this.ionicForm = this.formBuilder.group({
+      weight: ['', [Validators.required, Validators.minLength(2)]] ,
+      bweight: ['', [Validators.required, Validators.minLength(2)]] 
+    })
     
   } 
   
@@ -22,6 +30,32 @@ export class CalculatorPage implements OnInit {
       buttons: ['OK']
     });
 
+  }
+ 
+
+  async presentAlertConfirm(form) {
+   
+     var weight= parseInt(form.value['weight']);
+     var bweight= parseInt(form.value['bweight']);
+//Multiply your body weight in kilograms (i.e. 60) by 0.035. Eg. 0.035 x 60 = 2.1
+//Square your velocity (or speed) in metres per second, i.e. multiply it by itself. Eg. 1.4 x 1.4 = 1.96
+//Divide the result by your height in metres (i.e. 1.6). Eg.1.96 ÷ 1.6 = 1.225
+     var calories = weight+bweight;
+  
+    const alert = await this.alertController.create({ 
+      header: 'Calories Info!',
+      message: '  <strong> Calories : '+calories+'</strong>!!!',
+      buttons: [
+        {
+          text: 'Okay',
+          handler: () => {
+            console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 
 }
