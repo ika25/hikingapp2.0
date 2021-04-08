159fb0499c55c2be8bf73adc6831baf152b74e2c
ika25 <lomiika1990@yahoo.com>
2021-04-08 14:37:25 +0100

    installed chart.js packages for admin page

---
+++ hikingapp-2.0/src/app/pages/adminchart/adminchart.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adminchart',
+  templateUrl: './adminchart.page.html',
+  styleUrls: ['./adminchart.page.scss'],
+})
+export class AdminchartPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
