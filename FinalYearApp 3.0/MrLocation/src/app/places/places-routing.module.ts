4a8b254d90f741ae3703cf05378868ca01fe6eee
ika25 <lomiika1990@yahoo.com>
2021-08-08 19:07:44 +0100

    Added Tabs to the App

--- FinalYearApp 3.0/MrLocation/src/app/places/places-routing.module.ts
+++ FinalYearApp 3.0/MrLocation/src/app/places/places-routing.module.ts
@@ -1,25 +1,63 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { PlacesPage } from './places.page';
 
 const routes: Routes = [
   {
+    path: 'tabs',
+    component: PlacesPage,
+    children: [
+      {
+        path: 'discover',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('./discover/discover.module').then(m => m.DiscoverPageModule)
+          },
+          {
+            path: ':placeId',
+            loadChildren: () => import('./discover/place-detail/place-detail.module').then(m => m.PlaceDetailPageModule)
+          }
+        ]
+      },
+      {
+        path: 'offers',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('./finds/finds.module').then(m => m.FindsPageModule)
+          },
+          {
+            path: 'new',
+            loadChildren: () => import('./finds/new-finds/new-finds.module').then(m => m.NewFindsPageModule)
+          },
+          {
+            path: 'edit/:placeId',
+            loadChildren: () => import('./finds/edit-finds/edit-finds.module').then(m => m.EditFindsPageModule)
+          },
+          {
+            path: ':placeId',
+            loadChildren: () => import('./finds/find-bookmarks/find-bookmarks.module').then(m => m.FindBookmarksPageModule)
+          }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: '/places/tabs/discover',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
     path: '',
-    component: PlacesPage
-  },
-  {
-    path: 'discover',
-    loadChildren: () => import('./discover/discover.module').then( m => m.DiscoverPageModule)
-  },
-  {
-    path: 'finds',
-    loadChildren: () => import('./finds/finds.module').then( m => m.FindsPageModule)
+    redirectTo: '/places/tabs/discover',
+    pathMatch: 'full'
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class PlacesPageRoutingModule {}
+export class PlacesRoutingModule {}
