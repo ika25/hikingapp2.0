8cebcbe480b660e8b92d7a593c34e0a3782a46cb
ika25 <lomiika1990@yahoo.com>
2021-08-08 23:25:10 +0100

    Navigating between pages correctly.

--- FinalYearApp 3.0/MrLocation/src/app/places/discover/place-detail/place-detail.page.ts
+++ FinalYearApp 3.0/MrLocation/src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { NavController } from '@ionic/angular';
+
+import { PlacesService } from '../../places.service';
+import { Place } from '../../place.model'; // here we import place from place.model
 
 @Component({
   selector: 'app-place-detail',
@@ -8,13 +11,33 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./place-detail.page.scss']
 })
 export class PlaceDetailPage implements OnInit {
-  constructor(private router: Router, private navCtrl: NavController) {}
+  place: Place; //Load place
 
-  ngOnInit() {}
+  constructor(
+    private navCtrl: NavController,
+    private route: ActivatedRoute,
+    private placesService: PlacesService
+  ) {}
+
+
+  /*
+  This function will automatically update even if you are coming back from anther page that did change
+  that selcted place ID because that subscription that listens to change in the URLs will always be live
+  */
+  ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('placeId')) {
+        this.navCtrl.navigateBack('/places/tabs/discover');
+        return;
+      }
+      this.place = this.placesService.getPlace(paramMap.get('placeId'));
+    });
+  }
 
   onBookPlace() {
     // this.router.navigateByUrl('/places/tabs/discover');
-    this.navCtrl.navigateBack('/places/tabs/discover'); // navigating back page
+    this.navCtrl.navigateBack('/places/tabs/discover');
     // this.navCtrl.pop();
   }
 }
+
--- FinalYearApp 3.0/MrLocation/src/app/places/offers/edit-offer/edit-offer.page.ts
+++ FinalYearApp 3.0/MrLocation/src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NavController } from '@ionic/angular';
+
+import { PlacesService } from '../../places.service';
+import { Place } from '../../place.model';
 
 @Component({
   selector: 'app-edit-offer',
   templateUrl: './edit-offer.page.html',
-  styleUrls: ['./edit-offer.page.scss'],
+  styleUrls: ['./edit-offer.page.scss']
 })
 export class EditOfferPage implements OnInit {
+  place: Place;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private placesService: PlacesService,
+    private navCtrl: NavController
+  ) {}
 
+
+  /*
+  This function will automatically update even if you are coming back from anther page that did change
+  that selcted place ID because that subscription that listens to change in the URLs will always be live
+  */
   ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('placeId')) {
+        this.navCtrl.navigateBack('/places/tabs/offers');
+        return;
+      }
+      this.place = this.placesService.getPlace(paramMap.get('placeId'));
+    });
   }
-
 }
